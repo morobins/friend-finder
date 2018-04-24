7a63ff19534a16f5e3a55eef1c7c96f035431cf3
Monique Robinson <monique.robinson2@gmail.com>
2018-04-24 19:45:32 -0400

    Added the framework for the front end HTML

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+
 // Determine the user's most compatible friend using the following as a guide:
 
 // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
