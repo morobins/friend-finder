5808a20e5273dccb3c7cd1f89d30f91cf4eedff3
Monique Robinson <monique.robinson2@gmail.com>
2018-04-23 23:07:03 -0400

    added dummy data for friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,7 +16,7 @@ module.exports = [{
   },
   {
     "name": "Beyonce",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://fortunedotcom.files.wordpress.com/2016/04/gettyimages-88628374.jpg",
     "scores": [
       5,
       1,
@@ -32,7 +32,7 @@ module.exports = [{
   },
   {
     "name": "Rihanna",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://www.billboard.com/files/media/Rihanna-Love-On-The-Brain-2016-billboard-1548.jpg",
     "scores": [
       5,
       1,
@@ -48,7 +48,7 @@ module.exports = [{
   },
   {
     "name": "Bruno Mars",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/brunomars-hero_0.jpg?itok=vXKSuzEr",
     "scores": [
       5,
       1,
@@ -64,7 +64,7 @@ module.exports = [{
   },
   {
     "name": "Tupac",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://d3c1jucybpy4ua.cloudfront.net/data/53057/big_picture/Tupac.jpg?1491819908",
     "scores": [
       5,
       1,
@@ -80,7 +80,7 @@ module.exports = [{
   },
   {
     "name": "Pink",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://www.ocweekly.com/wp-content/uploads/2018/03/pinktickets.jpg",
     "scores": [
       5,
       1,
@@ -96,7 +96,7 @@ module.exports = [{
   },
   {
     "name": "Taylor Swift",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://media.glamour.com/photos/59fb504413a97d0f27f40315/master/w_644,c_limit/TAYLOR-SWIFT.jpg",
     "scores": [
       5,
       1,
@@ -112,7 +112,7 @@ module.exports = [{
   },
   {
     "name": "Cardi B",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://media.glamour.com/photos/5a4d072d759bef6ac14e61f0/master/w_644,c_limit/cardi.jpg",
     "scores": [
       5,
       1,
@@ -128,7 +128,7 @@ module.exports = [{
   },
   {
     "name": "Biggie",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "http://www.rap-up.com/app/uploads/2018/03/biggie.jpg",
     "scores": [
       5,
       1,
